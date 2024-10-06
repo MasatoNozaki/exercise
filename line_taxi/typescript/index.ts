048ac57bca01948608bb2047c370560f966ba04b
MasatoNozaki <noza4ki@gmail.com>
2024-10-06 15:27:31 +0900

    中断：トップダウンでやっていく

--- line_taxi/typescript/index.ts
+++ line_taxi/typescript/index.ts
@@ -1,7 +1,23 @@
+import { calculateFee } from "./src/fee";
+import { DistanceMeter, LowSpeedTimeMeter, type record } from "./src/meters";
 
+const distanceMeter = new DistanceMeter();
+const lowSpeedTimeMeter = new LowSpeedTimeMeter();
+
+const update = (record: record) => {
+    distanceMeter.update(record.distance);
+    lowSpeedTimeMeter.update(record);
+};
 
 export const answer = (input: string) => {
-    console.log(input);
+    const lines = input.split('\n');
+    for (const line of lines) {
+        const record = convert_to_record(line);
+        update(record);
+    }
+    // やっぱり累積じゃないとだめだわ！
+    // トップダウンでやっていきましょう
+    console.log(calculateFee(distanceMeter.distance, lowSpeedTimeMeter.cumulativeTime,));
     return 0;
 };
 
