ad16931b60a4e3ca47d249655428a396cbd84977
MasatoNozaki <noza4ki@gmail.com>
2024-10-14 17:16:52 +0900

    経過レコードを定義

--- line_taxi/typescript/src/lowSpeedTimeMeter.ts
+++ line_taxi/typescript/src/lowSpeedTimeMeter.ts
@@ -1,24 +1,29 @@
 import type { Temporal } from "temporal-polyfill";
 
 // 23時59分59秒999から0時に戻ると、低速走行時間メーターからすると時刻が連続しているという確証が取れない
-// レコード記録時点からの経過時間を表すフィールドをRecordに追加することで対応
-// LowSpeedTimeMeterは経過時間を扱う
+// レコード記録時点からの経過時間を表すレコードをRecordに追加することで対応
+// LowSpeedTimeMeterは経過レコードを扱う
 export type record = {
     time: Temporal.PlainTime;
+    elapsed: elapsedRecord;
+};
+
+export type elapsedRecord = {
     elapsedSecond: number;
     distance: number;
 };
+
 export class LowSpeedTimeMeter {
     cumulativeTime: number = 0;
-    private beforeElapsed: record['elapsedSecond'] | null = null;
-    update = (record: record): void => {
+    private beforeElapsed: elapsedRecord['elapsedSecond'] | null = null;
+    update = (record: elapsedRecord): void => {
         this.cumulativeTime += this.newLowSpeedTime(record);
         this.updateBeforeRecord(record.elapsedSecond);
     };
-    private updateBeforeRecord = (time: record['elapsedSecond']): void => {
+    private updateBeforeRecord = (time: elapsedRecord['elapsedSecond']): void => {
         this.beforeElapsed = time;
     };
-    private newLowSpeedTime = (record: record): number => {
+    private newLowSpeedTime = (record: elapsedRecord): number => {
         if (this.beforeElapsed != null) {
             const difference = record.elapsedSecond - this.beforeElapsed;
             if (difference < 0) {
--- line_taxi/typescript/test/lowSpeedTimeMeter.test.ts
+++ line_taxi/typescript/test/lowSpeedTimeMeter.test.ts
@@ -1,28 +1,23 @@
 import { describe, expect, test } from "bun:test";
-import { LowSpeedTimeMeter, TimeBackError, type record } from "../src/lowSpeedTimeMeter";
-import { Temporal } from "temporal-polyfill";
+import { LowSpeedTimeMeter, TimeBackError, type elapsedRecord } from "../src/lowSpeedTimeMeter";
 
 describe('低速走行時間メーター', () => {
     test('低速走行時間を加算する', () => {
-        const inputs: record[] = [
+        const inputs: elapsedRecord[] = [
             {
-                time: Temporal.PlainTime.from("00:00:00.000"),
                 elapsedSecond: 0,
                 distance: 0
             },
             {
-                time: Temporal.PlainTime.from("00:01:00.000"),
                 elapsedSecond: 60,
                 distance: 10
             },
             // 低速走行でない
             {
-                time: Temporal.PlainTime.from("00:01:30.000"),
                 elapsedSecond: 90,
                 distance: 120
             },
             {
-                time: Temporal.PlainTime.from("00:02:00.000"),
                 elapsedSecond: 120,
                 distance: 10
             },
@@ -36,42 +31,13 @@ describe('低速走行時間メーター', () => {
         expect(lowSpeedTimeMeter.cumulativeTime).toBe(90);
     });
 
-    test('時刻が一周しても低速走行時間が記録できる', () => {
-        const inputs: record[] = [
-            {
-                time: Temporal.PlainTime.from("23:59:00.000"),
-                elapsedSecond: 0,
-                distance: 0
-            },
-            {
-                time: Temporal.PlainTime.from("00:01:00.000"),
-                elapsedSecond: 120,
-                distance: 10
-            },
-            {
-                time: Temporal.PlainTime.from("00:02:00.000"),
-                elapsedSecond: 180,
-                distance: 10
-            },
-        ];
-
-        const lowSpeedTimeMeter = new LowSpeedTimeMeter();
-        for (const record of inputs) {
-            lowSpeedTimeMeter.update(record);
-        }
-
-        expect(lowSpeedTimeMeter.cumulativeTime).toBe(180);
-    });
-
     test('時刻が逆戻りしたらエラーを出す', () => {
-        const inputs: record[] = [
+        const inputs: elapsedRecord[] = [
             {
-                time: Temporal.PlainTime.from("00:01:00.000"),
                 elapsedSecond: 0,
                 distance: 0
             },
             {
-                time: Temporal.PlainTime.from("00:00:00.000"),
                 elapsedSecond: -60,
                 distance: 10
             },
