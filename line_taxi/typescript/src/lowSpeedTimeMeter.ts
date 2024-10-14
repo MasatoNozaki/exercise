aba589d77d34dda8fce4b9ce30d5d488d730aa08
MasatoNozaki <noza4ki@gmail.com>
2024-10-14 17:13:35 +0900

    経過時間フィールドを追加し、時刻一周に対応

--- line_taxi/typescript/src/lowSpeedTimeMeter.ts
+++ line_taxi/typescript/src/lowSpeedTimeMeter.ts
@@ -1,32 +1,33 @@
 import type { Temporal } from "temporal-polyfill";
 
+// 23時59分59秒999から0時に戻ると、低速走行時間メーターからすると時刻が連続しているという確証が取れない
+// レコード記録時点からの経過時間を表すフィールドをRecordに追加することで対応
+// LowSpeedTimeMeterは経過時間を扱う
 export type record = {
     time: Temporal.PlainTime;
+    elapsedSecond: number;
     distance: number;
 };
-
 export class LowSpeedTimeMeter {
     cumulativeTime: number = 0;
-    private beforeTime: Temporal.PlainTime | null = null;
+    private beforeElapsed: record['elapsedSecond'] | null = null;
     update = (record: record): void => {
         this.cumulativeTime += this.newLowSpeedTime(record);
-        this.updateBeforeRecord(record.time);
+        this.updateBeforeRecord(record.elapsedSecond);
     };
-    private updateBeforeRecord = (time: Temporal.PlainTime): void => {
-        this.beforeTime = time;
+    private updateBeforeRecord = (time: record['elapsedSecond']): void => {
+        this.beforeElapsed = time;
     };
     private newLowSpeedTime = (record: record): number => {
-        if (this.beforeTime != null) {
-            // 45秒単位で計算することになるので、秒変換
-            const difference = record.time.since(this.beforeTime).total('second');
+        if (this.beforeElapsed != null) {
+            const difference = record.elapsedSecond - this.beforeElapsed;
+            if (difference < 0) {
+                throw new TimeBackError(`Time is back. result: ${difference}`);
+            }
             // 時速10km以下なら
             const km = record.distance / 1000;
             const hour = difference / 60 / 60;
             if (km / hour < 10) {
-                // 結果が負の場合。1日経った結果なのか、2日経った結果なのかが不明で経過時間が定義できないためエラー
-                if (difference < 0) {
-                    throw new TimeBackError(`Time is back. result: ${difference}`);
-                }
                 return difference;
             }
         }
@@ -38,5 +39,6 @@ export class LowSpeedTimeMeter {
 export class TimeBackError extends Error {
     public constructor(message?: string) {
         super(message);
+        this.name = 'TimeBackError';
     }
 }
--- line_taxi/typescript/test/lowSpeedTimeMeter.test.ts
+++ line_taxi/typescript/test/lowSpeedTimeMeter.test.ts
@@ -1,25 +1,29 @@
 import { describe, expect, test } from "bun:test";
-import { LowSpeedTimeMeter, type record } from "../src/lowSpeedTimeMeter";
+import { LowSpeedTimeMeter, TimeBackError, type record } from "../src/lowSpeedTimeMeter";
 import { Temporal } from "temporal-polyfill";
 
 describe('低速走行時間メーター', () => {
     test('低速走行時間を加算する', () => {
         const inputs: record[] = [
             {
-                time: Temporal.PlainTime.from("00:00:00"),
+                time: Temporal.PlainTime.from("00:00:00.000"),
+                elapsedSecond: 0,
                 distance: 0
             },
             {
-                time: Temporal.PlainTime.from("00:01:00"),
+                time: Temporal.PlainTime.from("00:01:00.000"),
+                elapsedSecond: 60,
                 distance: 10
             },
             // 低速走行でない
             {
-                time: Temporal.PlainTime.from("00:01:30"),
+                time: Temporal.PlainTime.from("00:01:30.000"),
+                elapsedSecond: 90,
                 distance: 120
             },
             {
-                time: Temporal.PlainTime.from("00:02:00"),
+                time: Temporal.PlainTime.from("00:02:00.000"),
+                elapsedSecond: 120,
                 distance: 10
             },
         ];
@@ -30,5 +34,51 @@ describe('低速走行時間メーター', () => {
         }
 
         expect(lowSpeedTimeMeter.cumulativeTime).toBe(90);
+    });
+
+    test('時刻が一周しても低速走行時間が記録できる', () => {
+        const inputs: record[] = [
+            {
+                time: Temporal.PlainTime.from("23:59:00.000"),
+                elapsedSecond: 0,
+                distance: 0
+            },
+            {
+                time: Temporal.PlainTime.from("00:01:00.000"),
+                elapsedSecond: 120,
+                distance: 10
+            },
+            {
+                time: Temporal.PlainTime.from("00:02:00.000"),
+                elapsedSecond: 180,
+                distance: 10
+            },
+        ];
+
+        const lowSpeedTimeMeter = new LowSpeedTimeMeter();
+        for (const record of inputs) {
+            lowSpeedTimeMeter.update(record);
+        }
+
+        expect(lowSpeedTimeMeter.cumulativeTime).toBe(180);
+    });
+
+    test('時刻が逆戻りしたらエラーを出す', () => {
+        const inputs: record[] = [
+            {
+                time: Temporal.PlainTime.from("00:01:00.000"),
+                elapsedSecond: 0,
+                distance: 0
+            },
+            {
+                time: Temporal.PlainTime.from("00:00:00.000"),
+                elapsedSecond: -60,
+                distance: 10
+            },
+        ];
+
+        const lowSpeedTimeMeter = new LowSpeedTimeMeter();
+        lowSpeedTimeMeter.update(inputs[0]);
+        expect(() => lowSpeedTimeMeter.update(inputs[1])).toThrowError(TimeBackError);
     });
 });
