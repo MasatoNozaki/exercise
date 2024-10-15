3aeab375c8a5287c1b75ca039d3ba1b86d365027
MasatoNozaki <noza4ki@gmail.com>
2024-10-15 20:47:51 +0900

    料金計算で時刻を使わないように変更 overTimeRateは時刻からrateを算出する際に利用予定

--- line_taxi/typescript/test/feeMeter.test.ts
+++ line_taxi/typescript/test/feeMeter.test.ts
@@ -1,7 +1,72 @@
 import { describe, expect, test } from "bun:test";
+import type { feeInfo } from "../src/lowSpeedTimeMeter";
+import { Temporal } from "temporal-polyfill";
 
 test('1km未満ならいくら距離を加算しても400円で、1kmを超えると400mごとに40円が加算され、10kmを超えると350mごとに40円が加算される', () => {
+    const inputs: feeInfo[] = [
+        {
+            rate: 'normal',
+            elapsed: {
+                elapsedSecond: 0,
+                distance: 0.0
+            }
+        },
+        {
+            rate: 'normal',
+            elapsed: {
+                elapsedSecond: 60,
+                distance: 400
+            },
+        },
+        {
+            rate: 'normal', // ここまで1km未満
+            elapsed: {
+                elapsedSecond: 120,
+                distance: 599
+            },
+        },
+        {
+            rate: 'normal', // ここで1km到達
+            elapsed: {
+                elapsedSecond: 120.001,
+                distance: 1.0
+            },
+        },
+        {
+            rate: 'normal', // ここで40円加算
+            elapsed: {
+                elapsedSecond: 180.001,
+                distance: 400
+            },
+        },
+        {
+            rate: 'normal', // ここでは加算されない
+            elapsed: {
+                elapsedSecond: 240.001,
+                distance: 399
+            },
+        },
+        {
+            rate: 'normal', // ここで10200に到達。400mごとの加算が終了。計8800円加算
+            elapsed: {
+                elapsedSecond: 300.001,
+                distance: 8401
+            },
+        },
+        {
+            rate: 'normal', // ここで40円加算
+            elapsed: {
+                elapsedSecond: 360.001,
+                distance: 350
+            },
+        }
+    ];
 
+    const feeMeter = new FeeMeter();
+    for (const info of inputs) {
+        feeMeter.update(info);
+    }
+    expect(feeMeter.fee).toBe(9280);
 });
 
 test('低速走行時間45秒ごとに40円が加算される', () => {
