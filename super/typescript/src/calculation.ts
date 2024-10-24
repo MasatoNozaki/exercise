ac8bdbd9df798e7873237f2ee0066556e4c9389f
MasatoNozaki <noza4ki@gmail.com>
2024-10-24 19:32:10 +0900

    購入リストをオブジェクトに変更

--- super/typescript/src/calculation.ts
+++ super/typescript/src/calculation.ts
@@ -1,34 +1,41 @@
 import { ITEMS } from "../items";
 
-type buyContent = {
-    id: keyof typeof ITEMS;
-    num: number;
+/**
+ * {id: keyof typeof ITEMS, num: number}の配列では、同じ商品のレコードが複数含まれる可能性があり、処理の手間が増えるためidをkeyとするオブジェクトとしている
+ */
+type buyContents = {
+    [index: keyof typeof ITEMS]: number;
 };
 
-export function calcTotalPrice(buyContents: buyContent[]): number {
+export function calcTotalPrice(buyContents: buyContents): number {
     let sum = 0;
-    for (const buyContent of buyContents) {
-        if (buyContent.num < 0) {
+    for (const [id, buyNum] of Object.entries(buyContents)) {
+        if (buyNum < 0) {
             throw new NegativeNumError();
         }
 
-        const item = ITEMS[buyContent.id];
+        const item = ITEMS[Number(id)];
         if (item === undefined) {
             throw new IndexOutOfRangeError();
         }
-        sum += item.price * buyContent.num;
+        sum += item.price * buyNum;
     }
     return sum;
 }
 
-export function calcTotalPriceWithTax(buyContents: buyContent[]): number {
+export function calcTotalPriceWithTax(buyContents: buyContents): number {
     return calcTotalPrice(buyContents) * 1.08;
 }
 
-export function calcTotalPriceWithTabaccoTaxInclude(buyContents: buyContent[], totalFunc: (buyContents: buyContent[]) => number): number {
+export function calcTotalPriceWithTabaccoTaxInclude(buyContents: buyContents, totalFunc: (buyContents: buyContents) => number): number {
     const [tabaccoArray, restArray] = extractItem([6, 7], buyContents);
     return calcTotalPrice(tabaccoArray) + totalFunc(restArray);
 }
+
+// export function calcTotalPriceAppleThreeDiscount(buyContents: buyContent[], totalFunc: (buyContents: buyContent[]) => number): number {
+//     const [appleArray, restArray] = extractItem([1], buyContents);
+
+// }
 
 /**
  * 
@@ -36,18 +43,20 @@ export function calcTotalPriceWithTabaccoTaxInclude(buyContents: buyContent[], t
  * @param buyContents 購入データ
  * @returns 抽出した購入データ配列と、それ以外の購入データ配列
  */
-function extractItem(targets: (keyof typeof ITEMS)[], buyContents: buyContent[]): [buyContent[], buyContent[]] {
-    const extracted: buyContent[] = [];
+function extractItem(targets: (keyof typeof ITEMS)[], buyContents: buyContents): [buyContents, buyContents] {
+    const extracted: buyContents = {};
     // インデックスの末尾からのループのため、配列の長さが変わっても動作する
-    for (let i = buyContents.length - 1; i >= 0; i--) {
-        if (targets.includes(buyContents[i].id)) {
-            extracted.push(buyContents[i]);
-            buyContents.splice(i, 1);
+    for (const [id, buyNum] of Object.entries(buyContents)) {
+        if (targets.includes(Number(id))) {
+            extracted[Number(id)] = buyNum;
+            delete buyContents[Number(id)];
         }
     }
 
     return [extracted, buyContents];
 }
+
+// function integrateRecord()
 
 export class IndexOutOfRangeError extends Error {
     constructor() {
--- super/typescript/test/calculation.test.ts
+++ super/typescript/test/calculation.test.ts
@@ -5,90 +5,43 @@ const TAX = 1.08;
 
 describe('合計金額（税金なし）の計算', () => {
     test('商品を1種類買う', () => {
-        expect(calcTotalPrice(
-            [
-                { id: 1, num: 1 }
-            ]
-        )).toBe(100 * 1);
+        expect(calcTotalPrice({ 1: 1 })).toBe(100 * 1);
 
-        expect(calcTotalPrice(
-            [
-                { id: 5, num: 3 }
-            ]
-        )).toBe(400 * 3);
+        expect(calcTotalPrice({ 5: 3 })).toBe(400 * 3);
     });
 
     test('商品を2種類買う', () => {
-        expect(calcTotalPrice(
-            [
-                { id: 2, num: 1 },
-                { id: 6, num: 2 }
-            ]
-        )).toBe(40 + 420 * 2);
-    });
-
-    test('同じ商品のエントリーが2つある', () => {
-        expect(calcTotalPrice(
-            [
-                { id: 2, num: 1 },
-                { id: 2, num: 1 }
-            ]
-        )).toBe(40 + 40);
+        expect(calcTotalPrice({ 2: 1, 6: 2 })).toBe(40 + 420 * 2);
     });
 
     test('商品を1つも買わない', () => {
-        expect(calcTotalPrice([])).toBe(0);
+        expect(calcTotalPrice({})).toBe(0);
     });
 
     describe('存在しない商品を買う', () => {
         test('idが大きい', () => {
-            expect(() => calcTotalPrice(
-                [
-                    { id: 100, num: 1 }
-                ]
-            )).toThrowError(IndexOutOfRangeError);
+            expect(() => calcTotalPrice({ 100: 1 })).toThrowError(IndexOutOfRangeError);
         });
 
         test('idが負', () => {
-            expect(() => calcTotalPrice(
-                [
-                    { id: -1, num: 1 }
-                ]
-            )).toThrowError(IndexOutOfRangeError);
+            expect(() => calcTotalPrice({ '-1': 1 })).toThrowError(IndexOutOfRangeError);
         });
     });
 
     test('負の個数買う', () => {
-        expect(() => calcTotalPrice(
-            [
-                { id: 1, num: -1 }
-            ]
-        )).toThrowError(NegativeNumError);
+        expect(() => calcTotalPrice({ 1: -1 })).toThrowError(NegativeNumError);
     });
 });
 
 describe('合計金額（税金あり）の計算', () => {
     test('商品を1種類買う', () => {
-        expect(calcTotalPriceWithTax(
-            [
-                { id: 1, num: 1 }
-            ]
-        )).toBe(100 * 1 * TAX);
+        expect(calcTotalPriceWithTax({ 1: 1 })).toBe(100 * 1 * TAX);
 
-        expect(calcTotalPriceWithTax(
-            [
-                { id: 5, num: 3 }
-            ]
-        )).toBe(400 * 3 * TAX);
+        expect(calcTotalPriceWithTax({ 5: 3 })).toBe(400 * 3 * TAX);
     });
 
     test('商品を2種類買う', () => {
-        expect(calcTotalPriceWithTax(
-            [
-                { id: 2, num: 1 },
-                { id: 6, num: 2 }
-            ]
-        )).toBe((40 + 420 * 2) * TAX);
+        expect(calcTotalPriceWithTax({ 2: 1, 6: 2 })).toBe((40 + 420 * 2) * TAX);
     });
 
     test('商品を1つも買わない', () => {
@@ -96,54 +49,34 @@ describe('合計金額（税金あり）の計算', () => {
     });
 
     test('存在しない商品を買う', () => {
-        expect(() => calcTotalPriceWithTax(
-            [
-                { id: 100, num: 1 }
-            ]
-        )).toThrowError(IndexOutOfRangeError);
+        expect(() => calcTotalPriceWithTax({ 100: 1 })).toThrowError(IndexOutOfRangeError);
     });
 
     test('負の個数買う', () => {
-        expect(() => calcTotalPriceWithTax(
-            [
-                { id: 1, num: -1 }
-            ]
-        )).toThrowError(NegativeNumError);
+        expect(() => calcTotalPriceWithTax({ 1: -1 })).toThrowError(NegativeNumError);
     });
 });
 
 describe('タバコ金額が内税である場合の合計金額', () => {
     test('タバコを1つ買う', () => {
-        const data = [
-            { id: 6, num: 1 }
-        ];
+        const data = { 6: 1 };
 
         expect(calcTotalPriceWithTabaccoTaxInclude(data, calcTotalPriceWithTax)).toBe(420);
     });
 
     test('異なる種類のタバコを複数買う', () => {
-        const data = [
-            { id: 6, num: 1 },
-            { id: 7, num: 3 }
-        ];
+        const data = { 6: 1, 7: 3 };
 
         expect(calcTotalPriceWithTabaccoTaxInclude(data, calcTotalPriceWithTax)).toBe(420 + 440 * 3);
     });
 
     test('タバコでないもののみ買う(消費税あり)', () => {
-        const data = [
-            { id: 1, num: 2 },
-            { id: 5, num: 1 }
-        ];
+        const data = { 1: 2, 5: 1 };
         expect(calcTotalPriceWithTabaccoTaxInclude(data, calcTotalPriceWithTax)).toBe((100 * 2 + 400) * TAX);
     });
 
     test('タバコとそうでないものを買う(消費税なし)', () => {
-        const data = [
-            { id: 1, num: 2 },
-            { id: 5, num: 1 },
-            { id: 6, num: 1 }
-        ];
+        const data = { 1: 2, 5: 1, 6: 1 };
         expect(calcTotalPriceWithTabaccoTaxInclude(data, calcTotalPrice)).toBe(100 * 2 + 400 + 420);
     });
 });
