5b521d64ebf3ce37cd4d7206dee0c74e6f17495e
MasatoNozaki <noza4ki@gmail.com>
2024-10-25 21:09:26 +0900

    距離の数値の形式チェック

--- line_taxi/typescript_expand/src/checkDistanceRecordForm.ts
+++ line_taxi/typescript_expand/src/checkDistanceRecordForm.ts
@@ -1,12 +1,46 @@
 const SPLIT = '\n';
 
-export function checkDistanceRecordForm(record: string): boolean {
-    const distances = record.split(SPLIT);
+declare const distanceNominality: unique symbol;
+type distanceRecord = string & { [distanceNominality]: never; };
 
-    return checkFirstDistance(distances) && checkExistance_First_and_LastRecord(distances) && checkEndLine(distances);
+const isDistanceRecord = (line: string): line is distanceRecord => {
+    return /^(0|[1-9][0-9]?)\.[0-9]$/.test(line);
+};
+
+export function checkDistanceRecordForm(record: string): boolean {
+    if (!checkEndLine(record)) {
+        return false;
+    }
+
+    // 末尾の改行記号がdistanceRecord Typeに違反するためここで削除する
+    const recordTrimLastLF = record.trimEnd();
+
+    try {
+        const distances = checkDistanceRecordStyle(recordTrimLastLF);
+        return checkFirstDistance(distances) && checkExistance_First_and_LastRecord(distances);
+    } catch (e) {
+        if (e instanceof DistanceStyleError) {
+            return false;
+        }
+        else {
+            throw new Error('unknown error!');
+        }
+    }
 }
 
-function checkFirstDistance(distances: string[]): boolean {
+function checkDistanceRecordStyle(record: string): distanceRecord[] {
+    const lines = record.split(SPLIT);
+    for (const line of lines) {
+        if (!isDistanceRecord(line)) {
+            throw new DistanceStyleError(line);
+        }
+    }
+
+    // distanceRecordであることがすでに確かめられているのでasが許される
+    return lines as distanceRecord[];
+}
+
+function checkFirstDistance(distances: distanceRecord[]): boolean {
     if (distances[0] === '0.0') {
         return true;
     }
@@ -15,7 +49,7 @@ function checkFirstDistance(distances: string[]): boolean {
     }
 }
 
-function checkExistance_First_and_LastRecord(distances: string[]): boolean {
+function checkExistance_First_and_LastRecord(distances: distanceRecord[]): boolean {
     if (distances.length >= 2) {
         return true;
     }
@@ -24,11 +58,18 @@ function checkExistance_First_and_LastRecord(distances: string[]): boolean {
     }
 }
 
-function checkEndLine(distances: string[]): boolean {
-    if (distances[distances.length - 1] === "") {
+function checkEndLine(record: string): boolean {
+    const lines = record.split(SPLIT);
+    if (lines[lines.length - 1] === "") {
         return true;
     }
     else {
         return false;
+    }
+}
+
+class DistanceStyleError extends Error {
+    constructor(errorStyle: string) {
+        super(`Distance style error! min: 0.0, max: 99.9\nError style is ${errorStyle}`);
     }
 }
--- line_taxi/typescript_expand/test/index.test.ts
+++ line_taxi/typescript_expand/test/index.test.ts
@@ -15,14 +15,21 @@ describe('異常系', () => {
     });
 
     test('仕様と異なる区切り文字', () => {
-        expect(main('0.0$n1.0\n3.0')).toBeFalse();
+        expect(main('0.0$n1.0\n3.0\n')).toBeFalse();
     });
 
     test('1行のみである', () => {
-        expect(main('0.0')).toBeFalse();
+        expect(main('0.0\n')).toBeFalse();
     });
 
     test('末尾に改行がない', () => {
         expect(main('0.0\n1.0\n3.0')).toBeFalse();
+    });
+
+    test('距離レコード形式が異なる', () => {
+        expect(main('0.0\n1.00\n')).toBeFalse();
+        expect(main('0.0\nhoge\n')).toBeFalse();
+        expect(main('0.0\n100.0\n')).toBeFalse();
+        expect(main('0.0\n-1.0\n')).toBeFalse();
     });
 });
