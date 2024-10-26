6c797d80fe88cae62756be410c6ca6df824e5a8c
MasatoNozaki <noza4ki@gmail.com>
2024-10-26 11:02:01 +0900

    1kmを超えると40円加算する処理の実装

--- line_taxi/typescript_expand/src/distanceFee.ts
+++ line_taxi/typescript_expand/src/distanceFee.ts
@@ -1,5 +1,34 @@
 import type { distanceRecord } from "./checkDistanceRecordForm";
 
-export function calculateDistanceFee(distanceRecords: distanceRecord[]) {
-    return 400;
+/**
+ * distanceはm単位
+ * @param distanceRecords 距離レコードの文字列配列
+ * @returns 距離による総運賃
+ */
+export function calculateDistanceFee(distanceRecords: distanceRecord[]): number {
+    let totalFee = 0;
+    let totalDistance = getTotalDistance(distanceRecords);
+    // 初乗運賃
+    if (totalDistance > 0) {
+        totalFee += 400;
+        totalDistance -= 1000;
+    }
+
+    // 10km未満
+    while (totalDistance >= 0) {
+        totalFee += 40;
+        totalDistance -= 400;
+    }
+
+    return totalFee;
+}
+
+function getTotalDistance(distanceRecords: distanceRecord[]): number {
+    let sum = 0;
+    for (const distance of distanceRecords) {
+        sum += Number(distance);
+        sum = Math.round(sum * 10) / 10;
+    }
+
+    return sum;
 }
